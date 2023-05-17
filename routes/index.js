701e167ebeadf4f304e59b4cc2ee2d79b436bfd2
Ukasah Hayata <ukasahhayata@gmail.com>
2023-05-17 14:48:12 +0700

    edit routes/index

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const user = require('../controllers/user');
 
 const middlewares = require('../utils/middlewares');
 
+router.post('/', (req, res) => {
+    return res.status(200).json({
+        status: true,
+        message: "welcome to auth api",
+        data: null
+    })
+});
 router.post('/auth/register', user.register);
 router.post('/auth/login', user.login);
 router.get('/auth/whoami', middlewares.auth, user.whoami);
